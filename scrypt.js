ccac7d0999e3c78dcb296c15239420c42ebac027
DashkevichAS <antonsnov0604@gmail.com>
2023-02-02 14:11:44 +0300

    start

--- scrypt.js
+++ scrypt.js
@@ -10,23 +10,26 @@ function App() {
   const fifthRef = React.useRef(null);
   const sixthRef = React.useRef(null);
 
-  const [first, setFirst] = React.useState('');
-  const [firstCondition, setFirstCondition] = React.useState('');
+  const [value, setValue] = React.useState(['', '', '', '', '', '']);
+  const [condition, setCondition] = React.useState(['', '', '', '', '', '']);
 
-  const [second, setSecond] = React.useState('');
-  const [secondCondition, setSecondCondition] = React.useState('');
+  // const [first, setFirst] = React.useState('');
+  // const [firstCondition, setFirstCondition] = React.useState('');
 
-  const [third, setThird] = React.useState('');
-  const [thirdCondition, setThirdCondition] = React.useState('');
+  // const [second, setSecond] = React.useState('');
+  // const [secondCondition, setSecondCondition] = React.useState('');
 
-  const [fourth, setFourth] = React.useState('');
-  const [fourthCondition, setFourthCondition] = React.useState('');
+  // const [third, setThird] = React.useState('');
+  // const [thirdCondition, setThirdCondition] = React.useState('');
 
-  const [fifth, setFifth] = React.useState('');
-  const [fifthCondition, setFifthCondition] = React.useState('');
+  // const [fourth, setFourth] = React.useState('');
+  // const [fourthCondition, setFourthCondition] = React.useState('');
 
-  const [sixth, setSixth] = React.useState('');
-  const [sixthCondition, setSixthCondition] = React.useState('');
+  // const [fifth, setFifth] = React.useState('');
+  // const [fifthCondition, setFifthCondition] = React.useState('');
+
+  // const [sixth, setSixth] = React.useState('');
+  // const [sixthCondition, setSixthCondition] = React.useState('');
 
   const inputs = [
     firstRef.current,
@@ -37,18 +40,20 @@ function App() {
     sixthRef.current,
   ];
 
-  function controlFocusNextElem(n) {
-    for (let i = n; i < inputs.length; i++) {
-      if (!inputs[i]?.disabled) {
-        inputs[i]?.focus();
-        break;
-      }
-    }
-  }
+  // function controlFocusNextElem(n) {
+  //   for (let i = n; i < inputs.length; i++) {
+  //     if (!inputs[i]?.disabled) {
+  //       inputs[i]?.focus();
+  //       break;
+  //     }
+  //   }
+  // }
+  console.log('render');
 
   React.useEffect(() => {
     document.addEventListener('keydown', (e) => {
       if (e.key === 'Backspace') {
+        console.log(132);
         for (let i = inputs.length; i >= 0; i--) {
           if (inputs[i - 1]?.value && !inputs[i - 1]?.disabled) {
             inputs[i - 1]?.focus();
@@ -59,105 +64,134 @@ function App() {
     });
   });
 
-  React.useEffect(() => {
-    !sixth && fifthRef.current.focus();
-  }, [sixth]);
+  // React.useEffect(() => {
+  //   if (value) {
+  //     controlFocusNextElem(5);
+  //   } else {
+  //     fifthRef.current.focus();
+  //   }
+  // }, [value]);
 
-  React.useEffect(() => {
-    if (fifth) {
-      controlFocusNextElem(5);
-    } else {
-      fifthRef.current.focus();
-    }
-  }, [fifth]);
+  // React.useEffect(() => {
+  //   !sixth && fifthRef.current.focus();
+  // }, [sixth]);
 
-  React.useEffect(() => {
-    if (fourth) {
-      controlFocusNextElem(4);
-    } else {
-      fourthRef.current.focus();
-    }
-  }, [fourth]);
+  // React.useEffect(() => {
+  //   if (fifth) {
+  //     controlFocusNextElem(5);
+  //   } else {
+  //     fifthRef.current.focus();
+  //   }
+  // }, [fifth]);
 
-  React.useEffect(() => {
-    if (third) {
-      controlFocusNextElem(3);
-    } else {
-      thirdRef.current.focus();
-    }
-  }, [third]);
+  // React.useEffect(() => {
+  //   if (fourth) {
+  //     controlFocusNextElem(4);
+  //   } else {
+  //     fourthRef.current.focus();
+  //   }
+  // }, [fourth]);
 
-  React.useEffect(() => {
-    if (second) {
-      controlFocusNextElem(2);
-    } else {
-      secondRef.current.focus();
-    }
-  }, [second]);
+  // React.useEffect(() => {
+  //   if (third) {
+  //     controlFocusNextElem(3);
+  //   } else {
+  //     thirdRef.current.focus();
+  //   }
+  // }, [third]);
 
-  React.useEffect(() => {
-    if (first) {
-      controlFocusNextElem(1);
-    } else {
-      firstRef.current.focus();
-    }
-  }, [first]);
+  // React.useEffect(() => {
+  //   if (second) {
+  //     controlFocusNextElem(2);
+  //   } else {
+  //     secondRef.current.focus();
+  //   }
+  // }, [second]);
 
-  React.useEffect(() => {
-    firstRef.current.focus();
-  }, []);
+  // React.useEffect(() => {
+  //   if (first) {
+  //     controlFocusNextElem(1);
+  //   } else {
+  //     firstRef.current.focus();
+  //   }
+  // }, [first]);
 
-  React.useEffect(() => {
-    if (first && second && third && fourth && fifth && sixth) {
-      if (sixth !== '6') {
-        setSixth('');
-        setSixthCondition('invalid');
-      } else {
-        sixthRef.current.disabled = true;
-      }
+  // React.useEffect(() => {
+  //   firstRef.current.focus();
+  // }, []);
 
-      if (fifth !== '5') {
-        setFifth('');
-        setFifthCondition('invalid');
-      } else {
-        fifthRef.current.disabled = true;
-      }
+  // React.useEffect(() => {
+  //   if (first && second && third && fourth && fifth && sixth) {
+  //     if (sixth !== '6') {
+  //       setSixth('');
+  //       setSixthCondition('invalid');
+  //     } else {
+  //       sixthRef.current.disabled = true;
+  //     }
 
-      if (fourth !== '4') {
-        setFourth('');
-        setFourthCondition('invalid');
-      } else {
-        fourthRef.current.disabled = true;
-      }
+  //     if (fifth !== '5') {
+  //       setFifth('');
+  //       setFifthCondition('invalid');
+  //     } else {
+  //       fifthRef.current.disabled = true;
+  //     }
 
-      if (third !== '3') {
-        setThird('');
-        setThirdCondition('invalid');
-      } else {
-        thirdRef.current.disabled = true;
-      }
+  //     if (fourth !== '4') {
+  //       setFourth('');
+  //       setFourthCondition('invalid');
+  //     } else {
+  //       fourthRef.current.disabled = true;
+  //     }
 
-      if (second !== '2') {
-        setSecond('');
-        setSecondCondition('invalid');
-      } else {
-        secondRef.current.disabled = true;
-      }
+  //     if (third !== '3') {
+  //       setThird('');
+  //       setThirdCondition('invalid');
+  //     } else {
+  //       thirdRef.current.disabled = true;
+  //     }
 
-      if (first !== '1') {
-        setFirst('');
-        setFirstCondition('invalid');
-      } else {
-        firstRef.current.disabled = true;
-      }
-    }
-  }, [first, second, third, fourth, fifth, sixth]);
+  //     if (second !== '2') {
+  //       setSecond('');
+  //       setSecondCondition('invalid');
+  //     } else {
+  //       secondRef.current.disabled = true;
+  //     }
 
-  console.log(fifthCondition);
+  //     if (first !== '1') {
+  //       setFirst('');
+  //       setFirstCondition('invalid');
+  //     } else {
+  //       firstRef.current.disabled = true;
+  //     }
+  //   }
+  // }, [first, second, third, fourth, fifth, sixth]);
+
+  // console.log(fifthCondition);
   return (
     <section>
       <form className='form'>
-        <input
+        {[...new Array(6)].map((_, i) => (
+          <input
+            key={i}
+            ref={inputs[i]}
+            className={condition[i] === 'invalid' ? 'input invalid' : 'input'}
+            type='text'
+            value={value[i]}
+            maxLength='1'
+            onChange={(e) =>
+              setValue(
+                value.map((item, index) => {
+                  if (i === index) {
+                    item = e.target.value.replace(/\D/, '');
+                  }
+                  return item;
+                }),
+              )
+            }
+          />
+        ))}
+
+        {/* <input
           ref={firstRef}
           className={firstCondition === 'invalid' ? 'input invalid' : 'input'}
           type='text'
@@ -204,7 +238,7 @@ function App() {
           value={sixth}
           maxLength='1'
           onChange={(e) => setSixth(e.target.value.replace(/\D/, ''))}
-        />
+        /> */}
       </form>
     </section>
   );
